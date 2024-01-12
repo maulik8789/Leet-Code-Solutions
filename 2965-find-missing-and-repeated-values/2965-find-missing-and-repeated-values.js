4d8cae9e2885cf05ca9a1955c266a362c243dd61
Maulik Vaghela <maulik.vagh@gmail.com>
2024-01-11 19:26:37 -0500

    Time: 122 ms (22.88%), Space: 45.6 MB (69.84%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.js
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.js
@@ -8,14 +8,10 @@ var findMissingAndRepeatedValues = function(grid) {
     let i;
     let grid1=[].concat(...grid);
     l=grid1.length;
-    // console.log(grid1.length);
     let ob={};
     for (i=0; i<grid1.length; i++){
-        // if (isNaN(ob[grid1[i]])){
-        // ob[grid1[i]] =0;
-        // }
+
         ob[grid1[i]] = isNaN(ob[grid1[i]]) ? 1 : (ob[grid1[i]] + 1);
-        // console.log(ob);
         if(ob[grid1[i]]==2){
             o.unshift(grid1[i]);
         }
