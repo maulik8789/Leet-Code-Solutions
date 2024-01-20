c3b4fbe374829b024ef5866e110865bdb8e93a7f
Maulik Vaghela <maulik.vagh@gmail.com>
2024-01-20 13:57:54 -0500

    Time: 47 ms (85.56%), Space: 48.9 MB (5.20%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,31 +4,41 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    let isCounting=0;
-    let getNeedle='';
-    let firstOccur=-1;
-    let k=0
-    for (i=0;i<haystack.length;i++){
-        if (isCounting==0){
-            k=0;
-        }
-        for (j=k;j<needle.length;j++){
-            if(needle[j]==haystack[i]){
-                if (j==0) {firstOccur=i;}
-                isCounting=1;
-                getNeedle+=needle[j];
-                if(getNeedle==needle) {return firstOccur}
-                k+=1;
-                break
-            }
-            else{
-                isCounting=0;
-                if (getNeedle!=''){i=firstOccur;}
-                getNeedle='';
-                
-                break;
-            }
+    for (i=0;i<=haystack.length-needle.length;i++){
+        if (haystack.slice(i,i+needle.length)==needle){
+            return i;
         }
     }
-    return -1
+    return -1;
+    
+    
+    
+    //     Runtime: 60 ms
+//     let isCounting=0;
+//     let getNeedle='';
+//     let firstOccur=-1;
+//     let k=0
+//     for (i=0;i<haystack.length;i++){
+//         if (isCounting==0){
+//             k=0;
+//         }
+//         for (j=k;j<needle.length;j++){
+//             if(needle[j]==haystack[i]){
+//                 if (j==0) {firstOccur=i;}
+//                 isCounting=1;
+//                 getNeedle+=needle[j];
+//                 if(getNeedle==needle) {return firstOccur}
+//                 k+=1;
+//                 break
+//             }
+//             else{
+//                 isCounting=0;
+//                 if (getNeedle!=''){i=firstOccur;}
+//                 getNeedle='';
+                
+//                 break;
+//             }
+//         }
+//     }
+//     return -1
 };
