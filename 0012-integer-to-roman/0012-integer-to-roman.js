256b10ec8821281df73a20e1c82c8b7d31ef06e5
Maulik Vaghela <maulik.vagh@gmail.com>
2024-01-21 17:31:49 -0500

    Time: 184 ms (13.62%), Space: 58.6 MB (5.08%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function(num) {
+    let o={
+        'I':1,
+        'IV':4,
+        'V':5,
+        'IX':9,
+        'X':10,
+        'XL':40,
+        'L':50,
+        'XC':90,
+        'C':100,
+        'CD':400,
+        'D':500,
+        'CM':900,
+        'M':1000
+    }
+    
+    let ans='';
+    let k=Object.keys(o);
+    while(num>0){
+        for (let i=0; i<k.length;i++){
+            if(o[k[i]]>num){
+           console.log(k[i]);
+                ans+=k[i-1];
+                num=num-o[k[i-1]];
+                break;
+                if (num==0){
+                    break;
+                }
+            }
+            else if(o[k[k.length-1]]<=num){
+                ans+=k[k.length-1];
+                num=num-o[k[k.length-1]];
+                break;
+            }
+        }    
+    }
+    return ans;
+};
