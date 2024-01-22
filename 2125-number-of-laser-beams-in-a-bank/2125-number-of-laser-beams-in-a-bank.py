90542f32665107f37ae66f8f9192797980bffb3b
Maulik Vaghela <maulik.vagh@gmail.com>
2024-01-22 17:06:18 -0500

    Time: 191 ms (28.35%), Space: 18.7 MB (99.24%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
@@ -5,15 +5,11 @@ class Solution:
         ans=0
         
         for i in range(len(bank)):
-            # with these lines, Runtime = 190ms around 25% faster than other esubmissions
-            # if '1' not in bank[i]:
-            #     continue
-            
+            if '1' not in bank[i]:
+                continue
             for j in range(len(bank[i])):
                 if bank[i][j]=='1':
                     m+=1
-            if m==0:
-                continue
             ans+=count*m
             count=m
             m=0
