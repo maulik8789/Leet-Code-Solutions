948b8ee413eb32f648d87c51b8e7f8fd90f6ba36
Maulik Vaghela <maulik.vagh@gmail.com>
2024-01-23 22:36:54 -0500

    Time: 937 ms (33.62%), Space: 38.7 MB (91.81%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.py
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.py
@@ -1,10 +1,8 @@
 class Solution:
     def garbageCollection(self, garbage: List[str], travel: List[int]) -> int:
-        colM,colP,colG=0,0,0
+        colP=0
         lstM,lstP,lstG=0,0,0
-        
         tr=0
-        ans=0
         for i in range(len(garbage)-1,-1,-1):
             if 'P' in garbage[i] and lstP==0:
                 lstP=i
@@ -13,7 +11,6 @@ class Solution:
             if 'M' in garbage[i] and lstM==0:
                 lstM=i
             
-            print(lstP,lstG,lstM)
         for j in range(3):
             for i in range(len(garbage)):
                 if tr==0:
@@ -31,7 +28,5 @@ class Solution:
                             colP+=1
                 if i>0 and lst>=i:        
                     colP+=travel[i-1]
-                print(colP)
             tr+=1
-        print(colP)
         return colP
