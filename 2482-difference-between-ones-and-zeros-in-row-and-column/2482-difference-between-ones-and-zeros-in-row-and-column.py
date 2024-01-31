a26c8d517c25d51ee22c5165c022c2fd77e78968
Maulik Vaghela <maulik.vagh@gmail.com>
2024-01-31 17:58:10 -0500

    Time: 2667 ms (5.04%), Space: 75 MB (5.01%) - LeetHub

---
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
@@ -0,0 +1,25 @@
+class Solution:
+    def onesMinusZeros(self, grid: List[List[int]]) -> List[List[int]]:
+        diff=[]
+        d=defaultdict(int)
+        oneRowKey='oneRow'
+        oneColKey='oneCol'
+        zeroRowKey='zeroRow'
+        zeroColKey='zeroCol'
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j]==1:
+                    d[f'{oneColKey}{j}']+=1
+                    d[f'{oneRowKey}{i}']+=1
+                else:
+                    d[f'{zeroRowKey}{i}']+=1
+                    d[f'{zeroColKey}{j}']+=1
+        print(d)
+            
+        for i in range(len(grid)):
+            diff.append([])
+            for j in range(len(grid[i])):
+                print(d[f'{oneRowKey}{i}'], d[f'{oneColKey}{j}'], d[f'{zeroRowKey}{i}'], d[f'{zeroRowKey}{j}'])
+                diff[i].append(d[f'{oneRowKey}{i}'] + d[f'{oneColKey}{j}'] - d[f'{zeroRowKey}{i}'] - d[f'{zeroColKey}{j}'] ) 
+        return diff
