e0abfd2d694b6a5bde5551488c179537445163e7
Maulik Vaghela <maulik.vagh@gmail.com>
2024-02-05 23:43:51 -0500

    Time: 45 ms (66.38%), Space: 16.7 MB (69.43%) - LeetHub

--- 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.py
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.py
@@ -5,24 +5,18 @@ class Solution:
         for i in range(len(s)):
             if s[i]==c:
                 ind.append(i)
-        print(ind)
         
         ans=[]
         j=0
         for i in range(len(s)):
-            # print(ind[j])
-            # print(ans)
-            # print(j)
             if i==ind[j]:
                 ans.append(ind[j]-i)
                 if len(ind)>1 and j<len(ind)-1:
                     j+=1
                     isFrst=1
             elif isFrst:
-                print(ind[j-1], ind[j-1])
                 ans.append(min(abs(i-ind[j-1]),abs(ind[j]-i)))
             else:
                 ans.append(abs(ind[j]-i))
-        print(ans)
         return ans
                 
