c5098c9767efbf7b520f61fd36c033d591e54ce4
Maulik Vaghela <maulik.vagh@gmail.com>
2024-02-07 20:43:12 -0500

    Time: 136 ms (35.27%), Space: 17.2 MB (73.98%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.py
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.py
@@ -1,24 +1,28 @@
 
 class Solution:
     def minPartitions(self, n: str) -> int:
-        # Increase the limit for integer string conversion
-        sys.set_int_max_str_digits(100000)
-        print(len(n))
-        tot=0
-        tmp=''
-        rep=0
+        ans=-1
+        # mathematical solution
+        for i in n:
+            if int(i)>ans:
+                ans=int(i)
+        return ans
+        # With actual logic: Runtime: 8406 ms
+#         # Increase the limit for integer string conversion
+#         sys.set_int_max_str_digits(100000)
+#         tot=0
+#         tmp=''
+#         rep=0
         
-        while n!='0':
-            for i in range(len(n)):
-                if n[i]!='0':
-                    tmp+='1'
-                else:
-                    tmp=tmp+'0'
-            # print(n)
-            # print(tmp)
-            tot=int(n)-int(tmp)
-            rep+=1
-            if tot==0:
-                return rep
-            tmp=''
-            n=str(tot)
+#         while n!='0':
+#             for i in range(len(n)):
+#                 if n[i]!='0':
+#                     tmp+='1'
+#                 else:
+#                     tmp=tmp+'0'
+#             tot=int(n)-int(tmp)
+#             rep+=1
+#             if tot==0:
+#                 return rep
+#             tmp=''
+#             n=str(tot)
