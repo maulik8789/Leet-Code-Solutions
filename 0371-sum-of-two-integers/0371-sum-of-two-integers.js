0af7c0783718baae17b3dde2da95decbda8f568a
Maulik Vaghela <maulik.vagh@gmail.com>
2024-02-11 12:05:36 -0500

    Time: 59 ms (14.53%), Space: 48.3 MB (40.00%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var getSum = function(a, b) {
+       while(b!=0)
+   {
+       let carry=a&b;
+       a=a^b;
+       b=carry<<1;
+       console.log(carry,b);
+   }
+   return a
+};
