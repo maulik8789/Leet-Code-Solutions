39bf6107930823024380a202754f0496a3d458d8
Maulik Vaghela <maulik.vagh@gmail.com>
2024-02-12 18:52:44 -0500

    Time: 65 ms (46.52%), Space: 51.4 MB (22.01%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -3,11 +3,9 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    let m=nums.length;
-    let arr=new Array(m+1).fill(-1);
+    let arr=new Array(nums.length+1).fill(-1);
     for (let i=0;i<nums.length;i++){
         arr[nums[i]]=nums[i];
     }
-    console.log(arr);
     return (arr.findIndex(element => element == -1));
 };
