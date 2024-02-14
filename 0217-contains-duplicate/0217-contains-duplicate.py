860a4cb7612c2e53e5e071ce2b90f5f33d6c515f
Maulik Vaghela <maulik.vagh@gmail.com>
2024-02-14 08:00:55 -0500

    Time: 454 ms (28.74%), Space: 34.8 MB (13.48%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        d= defaultdict(int)
+        for i in nums:
+            d[i]+=1
+            if d[i]==2:
+                return True
+        return False
