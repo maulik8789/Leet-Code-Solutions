a75a2b02a18c046faaaee9801dc5fd1e6b546ddc
Maulik Vaghela <maulik.vagh@gmail.com>
2024-02-16 15:55:30 -0500

    Time: 84 ms (85.93%), Space: 21 MB (86.42%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,23 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+        max1=0
+        d=defaultdict(int)
+        ans=[]
+        for i in nums:
+            d[i]+=1
+        if len(nums)==1 or (max(d.values())==min(d.values()) and max(d.values())==1):
+            return nums
+        if k==1:
+            return [key for key, value in d.items() if value == max(d.values())]
+        for i in range(max(d.values()),min(d.values()) - 1,-1):
+            
+            if k==0:
+                break
+            for key, value in d.items():
+                if value == i:
+                    ans.append(key)
+                    k-=1
+            print(ans)
+
+        return ans
