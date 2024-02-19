6c806bf4bbd6945d0e27a2a780845f8a04004aed
Maulik Vaghela <maulik.vagh@gmail.com>
2024-02-18 19:06:19 -0500

    Time: 91 ms (67.90%), Space: 16.5 MB (97.61%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,9 +1,18 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        res=[]
-        for i in range(9):
-            for j in range(9):
-                ele = board[i][j]
-                if ele!='.':
-                    res+=[(i,ele),(ele,j),(i//3,j//3,ele)]
-        return len(res)==len(set(res))
+        
+        ans=[]
+        for i, row in enumerate(board):
+            for j, x in enumerate(row):
+                if x!='.':
+                    ans+=[(i,x),(x,j),(i//3,j//3,x)]
+        return len(ans)==len(set(ans))
+        
+        # Runtime: 87 ms, faster than 85.09% of Python3 online submissions for Valid Sudoku.
+        # res=[]
+        # for i in range(9):
+        #     for j in range(9):
+        #         ele = board[i][j]
+        #         if ele!='.':
+        #             res+=[(i,ele),(ele,j),(i//3,j//3,ele)]
+        # return len(res)==len(set(res))
