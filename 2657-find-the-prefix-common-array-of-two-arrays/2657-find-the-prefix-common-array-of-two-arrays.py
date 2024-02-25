c9ad2dec8c089be5d7692085aa0d02654021e85d
Maulik Vaghela <maulik.vagh@gmail.com>
2024-02-24 20:56:17 -0500

    Time: 194 ms (14.62%), Space: 16.6 MB (84.58%) - LeetHub

--- 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.py
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.py
@@ -3,10 +3,22 @@ class Solution:
         ans=[]
         for i in range(len(A)):
             cm=0
-            for k in range(i+1):
-                for j in range(i+1):
-                    # print(i, j, B[:j+1])
-                    if A[k] == B[j]:
+            for j in range(i+1):
+                    # print(i, j, B[:i+1])
+                    if A[j] in B[:i+1]:
                         cm+=1
+                    # print(ans)
             ans.append(cm)
+            # print(ans)
         return (ans)
+        # Runtime: 445 ms, faster than 5.00% of Python3
+        # ans=[]
+        # for i in range(len(A)):
+        #     cm=0
+        #     for k in range(i+1):
+        #         for j in range(i+1):
+        #             # print(i, j, B[:j+1])
+        #             if A[k] == B[j]:
+        #                 cm+=1
+        #     ans.append(cm)
+        # return (ans)
