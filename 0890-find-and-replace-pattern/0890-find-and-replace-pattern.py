31e6abc07b1c1bffd5bcd11046b228267377a022
Maulik Vaghela <maulik.vagh@gmail.com>
2024-02-25 22:26:57 -0500

    Time: 35 ms (76.88%), Space: 16.5 MB (71.86%) - LeetHub

--- 0890-find-and-replace-pattern/0890-find-and-replace-pattern.py
+++ 0890-find-and-replace-pattern/0890-find-and-replace-pattern.py
@@ -1,5 +1,6 @@
 class Solution:
     def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
+        # gather letters' occurence in pattern
         d=defaultdict(list)
         for c in range(len(pattern)):
                d[pattern[c]].append(c)
