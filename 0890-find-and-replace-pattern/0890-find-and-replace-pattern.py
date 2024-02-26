402738e830f194cd85ae6c1b3b664e29f6efd7a2
Maulik Vaghela <maulik.vagh@gmail.com>
2024-02-25 22:25:07 -0500

    Time: 37 ms (60.93%), Space: 16.6 MB (71.86%) - LeetHub

--- 0890-find-and-replace-pattern/0890-find-and-replace-pattern.py
+++ 0890-find-and-replace-pattern/0890-find-and-replace-pattern.py
@@ -3,17 +3,12 @@ class Solution:
         d=defaultdict(list)
         for c in range(len(pattern)):
                d[pattern[c]].append(c)
-        print(d)
         ans=[]
         for w in words:
             temp=defaultdict(list)
             for l in range(len(w)):
                 temp[w[l]].append(l)
-            # print(list(temp.values()))
             if list(temp.values()) == list(d.values()):
                 ans.append(w)
-        print(ans)
         return ans
-            # for i in list(temp.values()):
-            #     if 
                 
