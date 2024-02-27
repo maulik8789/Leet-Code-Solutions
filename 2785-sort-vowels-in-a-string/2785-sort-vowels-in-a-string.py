e7f3f03d36240c0167e72d42412b2d1229ac3afa
Maulik Vaghela <maulik.vagh@gmail.com>
2024-02-26 22:52:14 -0500

    Time: 179 ms (35.66%), Space: 19.3 MB (69.58%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.py
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.py
@@ -1,22 +1,42 @@
 class Solution:
     def sortVowels(self, s: str) -> str:
-        d=defaultdict(int)
+        # d=defaultdict(int)
         vo=[]
-        ind=[]
+        # ind=[]
         v=['A','E','I','O','U','a','e','i','o','u']
         for l in range(len(s)):
             if s[l] in v:
                 vo.append(s[l])
-                ind.append(l)
+                # ind.append(l)
         if len(vo)==0:
             return s
         vo.sort()
         k=0
         s=list(s)
         for l in range(len(s)):
-            if k>len(vo)-1:
-                break
-            if l==ind[k]:
+            if s[l] in v:
                 s[l]=vo[k]
                 k+=1
         return ''.join(s)
+        
+        # # Runtime: 160 ms, faster than 45.54% of Python3
+        # d=defaultdict(int)
+        # vo=[]
+        # ind=[]
+        # v=['A','E','I','O','U','a','e','i','o','u']
+        # for l in range(len(s)):
+        #     if s[l] in v:
+        #         vo.append(s[l])
+        #         ind.append(l)
+        # if len(vo)==0:
+        #     return s
+        # vo.sort()
+        # k=0
+        # s=list(s)
+        # for l in range(len(s)):
+        #     if k>len(vo)-1:
+        #         break
+        #     if l==ind[k]:
+        #         s[l]=vo[k]
+        #         k+=1
+        # return ''.join(s)
