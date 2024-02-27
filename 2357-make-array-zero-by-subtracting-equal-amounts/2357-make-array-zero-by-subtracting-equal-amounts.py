432e4b21b654d803e2def0005bd8199c9bb586ec
Maulik Vaghela <maulik.vagh@gmail.com>
2024-02-27 13:01:12 -0500

    Time: 36 ms (74.62%), Space: 16.6 MB (43.86%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
@@ -1,14 +1,19 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        k=0
-        if max(nums)==0:
-            return 0
-        while max(nums)!=0:
-            non_zero_val = [x for x in nums if x != 0]
-            for i in range(len(nums)):
-                if nums[i]==0:
-                    continue
-                m=min(non_zero_val)
-                nums[i]=nums[i]-m
-            k+=1
-        return k
+        return len(set(nums)-{0})
+
+        
+        
+        # # Runtime: 85 ms, faster than 5.12% of Python3
+        # k=0
+        # if max(nums)==0:
+        #     return 0
+        # while max(nums)!=0:
+        #     non_zero_val = [x for x in nums if x != 0]
+        #     for i in range(len(nums)):
+        #         if nums[i]==0:
+        #             continue
+        #         m=min(non_zero_val)
+        #         nums[i]=nums[i]-m
+        #     k+=1
+        # return k
