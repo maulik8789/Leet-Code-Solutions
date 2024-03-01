e1e3f80543410d20bc57502358b54433d7ef32e2
Maulik Vaghela <maulik.vagh@gmail.com>
2024-02-29 22:52:05 -0500

    Time: 46 ms (53.76%), Space: 18.3 MB (21.87%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -3,10 +3,10 @@ import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         # Define a regex pattern to match special characters and spaces
-        if len(s)==1:
-            return True
-        if len(s)==0:
-            return False
+        # if len(s)==1:
+        #     return True
+        # if len(s)==0:
+        #     return False
 
         s=re.sub(r'[^a-zA-Z0-9]','',s.lower())
         return s==s[::-1]
