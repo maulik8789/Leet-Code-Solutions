c500a279e8763f98c89fc747d91e078850672fe0
Maulik Vaghela <maulik.vagh@gmail.com>
2024-02-29 22:44:22 -0500

    Time: 734 ms (5.01%), Space: 17.1 MB (75.58%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,6 +1,10 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         # Define a regex pattern to match special characters and spaces
+        if len(s)==1:
+            return True
+        if len(s)==0:
+            return False
         sc= "!@#$%^&*()_+{}[]:;<>,.?/~`\"'- "
         s=s.lower()
         for i in s:
