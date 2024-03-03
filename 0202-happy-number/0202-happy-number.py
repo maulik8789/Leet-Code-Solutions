77385afa915b6385fdeca14ab016a20e96dbd668
Maulik Vaghela <maulik.vagh@gmail.com>
2024-03-03 10:36:41 -0500

    Time: 34 ms (77.79%), Space: 16.7 MB (31.43%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def tot(num):
+            digs=0
+            while num>0:
+                # print('start',digs, num)
+                digs+= (num%10) * (num%10)
+                num=int(num/10)
+                # print(digs, num)
+            return digs
+        
+        sm=n
+        while sm!=1:
+            print(sm)
+            sm=tot(sm)
+            print('s',sm)
+            if sm==1:
+                return True
+            if sm==4:
+                return False
+        return True
