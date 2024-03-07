10e0082cee98b1a9ec18dd2f6bfb3803a49db797
Maulik Vaghela <maulik.vagh@gmail.com>
2024-03-07 18:06:03 -0500

    Time: 38 ms (69.32%), Space: 16.5 MB (95.44%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,3 +1,6 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
+#       get the integer values of both strings 
+#       and multiply them
+#       return its string value
         return str(int(num1)*int(num2))
