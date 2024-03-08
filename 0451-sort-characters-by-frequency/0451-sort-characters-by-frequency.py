d4bfd38b41033ff3c6b10e0ebea607c46950c2c5
Maulik Vaghela <maulik.vagh@gmail.com>
2024-03-08 09:11:26 -0500

    Time: 73 ms (9.28%), Space: 26.1 MB (8.51%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,16 +1,21 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
         freq=defaultdict(int)
+        
+        # add frequency as value to all letters in the string 
         for l in s:
             freq[l]+=1
         
+        # using a bucket
         bucket = [[] for _ in range(len(s)+1)]
         
+        # add letters on the index according to their frequency
         for ch, fr in freq.items():
             bucket[fr].append(ch)
             
         estr='' 
         
+        # starting from the most frequent letter, go backwards to the lowest
         for i in range(len(bucket)-1,-1,-1):
             if bucket[i]:
                 for ch in bucket[i]:
