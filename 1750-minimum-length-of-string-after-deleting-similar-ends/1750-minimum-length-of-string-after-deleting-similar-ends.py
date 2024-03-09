364aeafb38d0499567d4661fcb29bf5d61eee4e0
Maulik Vaghela <maulik.vagh@gmail.com>
2024-03-09 18:39:33 -0500

    Time: 73 ms (42.20%), Space: 17.5 MB (21.62%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.py
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.py
@@ -6,14 +6,14 @@ class Solution:
         while l-r>1 and s[r]==s[l-1]:
             # print(r,l)
                 # while s[r]==s[l-1] and l-r>1:
-            print(r,l-1)
+            # print(r,l-1)
             while s[l-1]==s[l-1-1] and l>0:
                 l-=1
             while s[r+1]==s[r] and r<l:
                 r+=1
             r+=1
             l-=1
-        print(r,l)
+        # print(r,l)
         if r==l-1:
             return 1
         return 0 if (s[l-1]==s[r] or l-r<0) else l-r
