4e9e9516aaea6a97f2b022c241c1ff4c0dcc330a
Maulik Vaghela <maulik.vagh@gmail.com>
2024-03-11 21:11:12 -0400

    Time: 24 ms (98.40%), Space: 16.7 MB (25.33%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.py
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -5,13 +5,12 @@ class Solution:
             if i in s:
                 ans+=i
         
-        for i in s:
-            if i not in ans:
-                ans+=i
-        print(ans)
+        # for i in s:
+            
         for i in range(len(s)):
+            if s[i] not in ans:
+                ans+=s[i]
             if s.count(ans[i])>ans.count(ans[i]):
                 ans = ans[:i] + ans[i] + ans[i:]
-        # ans = ans+''.join([char for char in s if s.count(char) > ans.count(char)])
         
         return ans
