cd734f302c981b7473208e80014d428baa397f30
Maulik Vaghela <maulik.vagh@gmail.com>
2024-03-12 22:54:18 -0400

    Time: 346 ms (58.81%), Space: 41.7 MB (20.87%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.py
+++ 2540-minimum-common-value/2540-minimum-common-value.py
@@ -1,8 +1,16 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        for i in range(len(nums1)):
-            if nums1[i] < nums2[0]:
-                continue
-            if nums1[i] in nums2:
-                return nums1[i]
+        d=Counter(nums1)
+        # print(d)
+        for i in nums2:
+            if i in d:
+                return i
         return -1
+            
+        
+        # for i in range(len(nums1)):
+        #     if nums1[i] < nums2[0]:
+        #         continue
+        #     if nums1[i] in nums2:
+        #         return nums1[i]
+        # return -1
