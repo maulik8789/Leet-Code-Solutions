1f7a542db9484994f1da4df918d10cdc4720ed40
Maulik Vaghela <maulik.vagh@gmail.com>
2024-03-20 17:09:46 -0400

    Time: 46 ms (7.29%), Space: 16.5 MB (85.37%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -6,20 +6,27 @@ import math
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        def getLen(header):
-            current=header
-            length=0
-            
-            while current:
-                length+=1
-                current=current.next
-            return length
+        slow=head
+        fast=head
+        while fast is not None and fast.next is not None:
+            slow=slow.next
+            fast=fast.next.next
+        return slow
         
-        lstLen=getLen(head)
-        curr=head
-        point=1
-        print((lstLen//2))
-        while point<=(lstLen//2):
-            point+=1
-            curr=curr.next
-        return curr
+#         def getLen(header):
+#             current=header
+#             length=0
+            
+#             while current:
+#                 length+=1
+#                 current=current.next
+#             return length
+        
+#         lstLen=getLen(head)
+#         curr=head
+#         point=1
+#         print((lstLen//2))
+#         while point<=(lstLen//2):
+#             point+=1
+#             curr=curr.next
+#         return curr
