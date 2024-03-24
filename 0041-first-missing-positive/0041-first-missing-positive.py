d6f51a2088ae288ee1cb7d7c6690b2b0d83548ca
Maulik Vaghela <maulik.vagh@gmail.com>
2024-03-24 11:34:31 -0400

    Time: 286 ms (69.63%), Space: 35.2 MB (14.12%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -2,11 +2,8 @@ class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         counter=0
         reqPos=1
-        # startPoint=min(nums)
-        print(list(set(nums)))
         nums=list(set(nums))
         nums.sort()
-        print(nums)
         i=0
         while i<len(nums):
             if nums[i]!=reqPos and nums[i]>0:
@@ -16,7 +13,6 @@ class Solution:
             else:
                 reqPos+=1
                 i+=1
-            print(i,reqPos)
         # while i<len(nums):
         #     if reqPos not in nums:
         #         return reqPos
