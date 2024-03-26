b51a3b18ebed3e807d763575f7ea04e83b72c8b2
Maulik Vaghela <maulik.vagh@gmail.com>
2024-03-26 00:08:50 -0400

    Time: 53 ms (5.54%), Space: 16.5 MB (69.28%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
@@ -6,41 +6,55 @@
 import sys
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head is None or head.next is None:
-            return head
-        if head.val==head.next.val and head.next.next is None:
-            return
-        dupVal=-sys.maxsize - 1
-        current=head
-        ans=None
-        res=None
-        while current:
-            if current.next:
-                if current.val==current.next.val:
-                    dupVal=current.next.val
-                    current=current.next
-                elif ans is None and current.val!=dupVal:
-                    newNode=ListNode(current.val)
-                    ans=newNode
-                    current=current.next
-                else:
-                    if res is None:
-                        res=ans
-                    if current.val!=dupVal:
-                        res.next=ListNode(current.val)
-                        res=res.next
-                    current=current.next
-            elif current.next is None and dupVal!=current.val:
-                if res is None:
-                    res=ans
-                if res is None:
-                    ans=ListNode(current.val)
-                    res=ans
-                    print(res, ans)
-                else:
-                    res.next=ListNode(current.val)
-                res=res.next
-                current=current.next
+        dummy = ListNode(0, head)
+        prev = dummy
+
+        while head and head.next:
+            if head.val == head.next.val:
+                while head and head.next and head.val == head.next.val:
+                    head = head.next
+                head = head.next
+                prev.next = head
             else:
-                current=current.next
-        return ans
+                prev = prev.next
+                head = head.next
+        return dummy.next
+    
+#         if head is None or head.next is None:
+#             return head
+#         if head.val==head.next.val and head.next.next is None:
+#             return
+#         dupVal=-sys.maxsize - 1
+#         current=head
+#         ans=None
+#         res=None
+#         while current:
+#             if current.next:
+#                 if current.val==current.next.val:
+#                     dupVal=current.next.val
+#                     current=current.next
+#                 elif ans is None and current.val!=dupVal:
+#                     newNode=ListNode(current.val)
+#                     ans=newNode
+#                     current=current.next
+#                 else:
+#                     if res is None:
+#                         res=ans
+#                     if current.val!=dupVal:
+#                         res.next=ListNode(current.val)
+#                         res=res.next
+#                     current=current.next
+#             elif current.next is None and dupVal!=current.val:
+#                 if res is None:
+#                     res=ans
+#                 if res is None:
+#                     ans=ListNode(current.val)
+#                     res=ans
+#                     print(res, ans)
+#                 else:
+#                     res.next=ListNode(current.val)
+#                 res=res.next
+#                 current=current.next
+#             else:
+#                 current=current.next
+#         return ans
