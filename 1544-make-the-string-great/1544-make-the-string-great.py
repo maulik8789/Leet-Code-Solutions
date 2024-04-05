142521ecb4ca401b4f1ae2155f7b71a13fa0e435
Maulik Vaghela <maulik.vagh@gmail.com>
2024-04-04 21:45:07 -0400

    Time: 45 ms (13.14%), Space: 16.6 MB (46.83%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -0,0 +1,19 @@
+class Solution:
+    def makeGood(self, s: str) -> str:
+        i=0
+        if len(s)==1:
+            return s
+        while i<(len(s)-1):
+            
+            if (s[i]==s[i+1].upper() and not s[i+1].isupper()) or (s[i+1]==s[i].upper() and not s[i].isupper()):
+                if i==len(s)-2:
+                    s=s[:i]
+                else:
+                    s=s[:i]+s[i+2:] 
+                print(s,i)
+                i-=1
+            else:
+                i+=1
+            if i<0:
+                i=0
+        return s
