e2cd18b0c192455dfd950b20b53fd11c6c88c610
Maulik Vaghela <maulik.vagh@gmail.com>
2024-04-05 21:04:41 -0400

    Time: 277 ms (7.08%), Space: 25.5 MB (17.22%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,16 +1,34 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # this problem is solved using suffix array
-        left=[1]*len(nums)
-        right=[1]*len(nums)
-        
-        for i in range(1,len(nums)):
-            left[i]=left[i-1]*nums[i-1]
-        for i in range(len(nums)-2,-1,-1):
-            right[i]=right[i+1]*nums[i+1]
+        res = [1]*len(nums)
+        left = 1
         for i in range(len(nums)):
-            nums[i]=left[i]*right[i]
-        return nums
+            res[i] = left
+            left *= nums[i]
+        right = 1
+        for i in range(len(nums)-1, -1, -1):
+            res[i] *= right
+            right *= nums[i]
+
+        return res 
+
+        # 1   1   2 3 4
+        #     1   2 3 4 1
+        #     1   1 2 6
+        #    24  12 8 6
+        
+        # this problem is solved using suffix array
+        # Runtime: 270ms
+#         left=[1]*len(nums)
+#         right=[1]*len(nums)
+        
+#         for i in range(1,len(nums)):
+#             left[i]=left[i-1]*nums[i-1]
+#         for i in range(len(nums)-2,-1,-1):
+#             right[i]=right[i+1]*nums[i+1]
+#         for i in range(len(nums)):
+#             nums[i]=left[i]*right[i]
+#         return nums
         
         
         # Time limit Exceeded
