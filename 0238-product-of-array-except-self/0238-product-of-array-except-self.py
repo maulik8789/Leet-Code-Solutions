18a4337970c364fcc7fe34ffd101ff93578b26db
Maulik Vaghela <maulik.vagh@gmail.com>
2024-04-05 21:02:26 -0400

    Time: 270 ms (10.19%), Space: 25.1 MB (19.25%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -6,10 +6,8 @@ class Solution:
         
         for i in range(1,len(nums)):
             left[i]=left[i-1]*nums[i-1]
-        print(left)
         for i in range(len(nums)-2,-1,-1):
             right[i]=right[i+1]*nums[i+1]
-        print(right)
         for i in range(len(nums)):
             nums[i]=left[i]*right[i]
         return nums
