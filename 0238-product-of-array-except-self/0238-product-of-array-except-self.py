7d20ae027ba034641f17cc9d1b735c407aaebb88
Maulik Vaghela <maulik.vagh@gmail.com>
2024-04-05 21:02:12 -0400

    Time: 288 ms (5.09%), Space: 25.3 MB (17.77%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -0,0 +1,28 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # this problem is solved using suffix array
+        left=[1]*len(nums)
+        right=[1]*len(nums)
+        
+        for i in range(1,len(nums)):
+            left[i]=left[i-1]*nums[i-1]
+        print(left)
+        for i in range(len(nums)-2,-1,-1):
+            right[i]=right[i+1]*nums[i+1]
+        print(right)
+        for i in range(len(nums)):
+            nums[i]=left[i]*right[i]
+        return nums
+        
+        
+        # Time limit Exceeded
+        # ans=1
+        # res=[]
+        # for i in range(len(nums)):
+        #     ans=1
+        #     for j in range(len(nums)):
+        #         if i==j:
+        #             continue
+        #         ans=ans*nums[j]
+        #     res.append(ans)
+        # return res
