229802d85ab7bab78338926e91d91a5a7bd68989
Maulik Vaghela <maulik.vagh@gmail.com>
2024-04-10 18:12:28 -0400

    Time: 51 ms (22.35%), Space: 16.8 MB (80.89%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.py
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.py
@@ -1,14 +1,18 @@
 from collections import deque
+from array import array
 class Solution:
     def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
         n = len(deck)
         deck.sort()
         st = deque()
         st.append(deck[n - 1])
+        # print(st)
         for i in range(n - 2, -1, -1):
             st.appendleft(st.pop())
             st.appendleft(deck[i])
+            # print(st)
         revealed = []
-        while st:
-            revealed.append(st.popleft())
-        return revealed
+        # while st:
+        #     revealed.append(st.popleft())
+        # return revealed        
+        return array('i',st)
