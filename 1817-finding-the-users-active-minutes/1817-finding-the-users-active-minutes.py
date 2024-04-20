fd4d350fa4ac26480cc1c95bec9e8c00a5a2d4cb
Maulik Vaghela <maulik.vagh@gmail.com>
2024-04-19 22:59:18 -0400

    Time: 724 ms (37.10%), Space: 26.7 MB (92.31%) - LeetHub

---
+++ 1817-finding-the-users-active-minutes/1817-finding-the-users-active-minutes.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findingUsersActiveMinutes(self, logs: List[List[int]], k: int) -> List[int]:
+        d=defaultdict(set)
+        for i in range(len(logs)):
+            d[logs[i][0]].add(logs[i][1])
+        print(d)
+        op=[0]*k
+        for i in d.values():
+            op[len(i)-1]+=1
+        return op
+        
