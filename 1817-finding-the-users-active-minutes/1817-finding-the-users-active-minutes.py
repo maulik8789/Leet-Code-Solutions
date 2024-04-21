c1c2421bbcb9368335276c4be665b769e8330425
Maulik Vaghela <maulik.vagh@gmail.com>
2024-04-20 20:28:34 -0400

    Time: 700 ms (75.45%), Space: 26.8 MB (92.27%) - LeetHub

--- 1817-finding-the-users-active-minutes/1817-finding-the-users-active-minutes.py
+++ 1817-finding-the-users-active-minutes/1817-finding-the-users-active-minutes.py
@@ -3,7 +3,6 @@ class Solution:
         d=defaultdict(set)
         for i in range(len(logs)):
             d[logs[i][0]].add(logs[i][1])
-        # print(d)
         op=[0]*k
         for i in d.values():
             op[len(i)-1]+=1
