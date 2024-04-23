d1a0f6fcc6720b7755cc569128836e678ec254e0
Maulik Vaghela <maulik.vagh@gmail.com>
2024-04-23 17:48:07 -0400

    Time: 38 ms (61.37%), Space: 16.6 MB (38.29%) - LeetHub

--- 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.py
+++ 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.py
@@ -1,6 +1,6 @@
 class Solution:
     def dominantIndex(self, nums: List[int]) -> int:
         maxNInd=nums.index(max(nums))
-        # maxN=nums.pop(maxNInd)
+        # maxN=nums.pop(maxNInd) 
         nums.sort()
         return maxNInd if nums[-1]>=nums[-2]*2 else -1
