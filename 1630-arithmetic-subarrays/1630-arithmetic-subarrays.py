2ec728cd0cbaaac4cae73e681a26931e7a5022c7
Maulik Vaghela <maulik.vagh@gmail.com>
2024-05-06 16:31:40 -0400

    Time: 272 ms (5.02%), Space: 17.6 MB (6.30%) - LeetHub

---
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.py
@@ -0,0 +1,22 @@
+class Solution:
+    def checkArithmeticSubarrays(self, nums: List[int], l: List[int], r: List[int]) -> List[bool]:
+        arr=[]
+        for i in range(len(l)):
+            print(l[i],r[i])
+            arr.append(sorted(nums[l[i]:r[i]+1]))
+        print(arr)
+        ans=[]
+        for i in range(len(arr)):
+            print(arr[i],arr[i])
+            diff=arr[i][0]-arr[i][1]
+            isFalse=0
+            for j in range(1,len(arr[i])):
+                if diff==arr[i][j-1]-arr[i][j]:
+                    continue
+                else:
+                    ans.append(False)
+                    isFalse=1
+                    break
+            if not isFalse:
+                ans.append(True)
+        return ans
