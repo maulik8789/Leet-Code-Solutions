e51fcf68d3396bb3dddfaa3e5400e308c200a77b
Maulik Vaghela <maulik.vagh@gmail.com>
2024-05-15 17:35:56 -0400

    Time: 65 ms (30.61%), Space: 16.8 MB (22.44%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,12 +1,22 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        if len(magazine)<len(ransomNote):
+            return False
         dR=defaultdict(int)
         dm=defaultdict(int)
-        for i in ransomNote:
-            dR[i]+=1
-        for i in magazine:
-            dm[i]+=1
-            
+        # for i in ransomNote:
+        #     dR[i]+=1
+        # for i in magazine:
+        #     dm[i]+=1
+        l1=len(magazine)
+        l2=len(ransomNote)
+        l=max(l1,l2)
+        for i in range(l):
+            if i<l1:
+                dm[magazine[i]]+=1
+            if i<l2:
+                dR[ransomNote[i]]+=1
+        print(dR, dm)
         for i in dR.keys():
             if dR[i]>dm[i] or i not in magazine:
                 return False
