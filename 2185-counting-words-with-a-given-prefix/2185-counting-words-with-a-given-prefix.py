da79704e86fe9973ce567b8eb85ae723bd6fb7df
Maulik Vaghela <maulik.vagh@gmail.com>
2024-06-11 08:47:57 -0400

    Time: 46 ms (43.39%), Space: 16.6 MB (70.56%) - LeetHub

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.py
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.py
@@ -1,6 +1,6 @@
 class Solution:
     def prefixCount(self, words: List[str], pref: str) -> int:
-        cnt=0
+        cnt=0 
         for w in words:
             if w[0]!=pref[0]:
                 next
