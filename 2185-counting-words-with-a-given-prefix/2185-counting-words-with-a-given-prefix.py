0b8cc146611a98f018fbe029ebce6c9cebe4c6a1
Maulik Vaghela <maulik.vagh@gmail.com>
2024-06-11 08:46:47 -0400

    Time: 48 ms (25.64%), Space: 16.7 MB (70.56%) - LeetHub

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.py
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.py
@@ -3,6 +3,8 @@ class Solution:
         p=len(pref)
         cnt=0
         for w in words:
+            if w[0]!=pref[0]:
+                next
             if w[:p]==pref:
                 cnt+=1
         return cnt
