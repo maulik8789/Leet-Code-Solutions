f4f47b0e6848dae8a1881b3f1f885f5514df1d97
Maulik Vaghela <maulik.vagh@gmail.com>
2024-07-08 10:37:15 -0400

    Time: 3880 ms (5.09%), Space: 16.3 MB (98.57%) - LeetHub

---
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        if n==1 or n==0:
+            return n
+        ans=[0]*(n)
+        i=0
+        x=1
+        c=0
+        while 0 in ans:
+            # print(ans)
+            if i==len(ans):
+                i=0
+            if x==k and ans[i] == 0:
+                ans[i]=1
+                c+=1
+                x=1
+            elif ans[i]==0:
+                x+=1
+            i+=1
+            # print(i, x)
+            
+            # print(ans)
+            if c==len(ans)-1:
+                break
+        
+
+        for l in range(len(ans)):
+            if ans[l]==0:
+                return l+1
+        return 100
