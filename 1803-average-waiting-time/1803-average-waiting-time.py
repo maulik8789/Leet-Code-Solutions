1f0be1a2454ef4717c1d41bcec2ca3a56eab0cee
Maulik Vaghela <maulik.vagh@gmail.com>
2024-07-09 14:28:02 -0400

    Time: 755 ms (43.64%), Space: 57.2 MB (9.97%) - LeetHub

--- 1803-average-waiting-time/1803-average-waiting-time.py
+++ 1803-average-waiting-time/1803-average-waiting-time.py
@@ -5,8 +5,6 @@ class Solution:
         t=c[0][0]
         cTime=0
         for i in range(l):
-            # if i==0:
-            #     t+=c[i][1]
             if t>=customers[i][0]:
                 t+=customers[i][1]
             elif i>0:
