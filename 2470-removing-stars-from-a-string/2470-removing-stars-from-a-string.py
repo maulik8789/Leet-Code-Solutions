a77b9464dcb60e6694bc53062c41d6b2957c83de
Maulik Vaghela <maulik.vagh@gmail.com>
2024-07-12 14:16:44 -0400

    Time: 115 ms (98.67%), Space: 18.3 MB (41.07%) - LeetHub

--- 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.py
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.py
@@ -1,19 +1,27 @@
 class Solution:
     def removeStars(self, s: str) -> str:
-        i=0
-        if '*' not in s:
-            return s
-        while i <len(s):
-            # print(i, s[i], s)
-            if s[i]=='*' and i!=len(s)-1:
-                s=s[:i-1]+s[i+1:]
-                i-=2
-                if i<0:
-                    i=0
-            elif s[i]=='*':
-                s=s[:i-1]
-                break
+        stack=[]
+        for char in s:
+            if char=='*':
+                stack.pop()
             else:
-                i+=1
+                stack.append(char)
+        return ''.join(stack)
+        # Runtime 3984ms Mem 17.6MB
+        # i=0
+        # if '*' not in s:
+        #     return s
+        # while i <len(s):
+        #     # print(i, s[i], s)
+        #     if s[i]=='*' and i!=len(s)-1:
+        #         s=s[:i-1]+s[i+1:]
+        #         i-=2
+        #         if i<0:
+        #             i=0
+        #     elif s[i]=='*':
+        #         s=s[:i-1]
+        #         break
+        #     else:
+        #         i+=1
 
-        return s
+        # return s
