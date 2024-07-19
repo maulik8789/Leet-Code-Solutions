c6d885c381258cb1d27e1a6f8af994046159ac49
Maulik Vaghela <maulik.vagh@gmail.com>
2024-07-18 23:17:58 -0400

    Time: 106 ms (77.12%), Space: 16.9 MB (43.01%) - LeetHub

--- 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.py
+++ 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.py
@@ -2,19 +2,14 @@ class Solution:
     def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
         arr=[]
         for i in range(len(matrix)):
-            print(min(matrix[i]))
-            print(matrix[i].index(min(matrix[i])))
             arr.append(matrix[i].index(min(matrix[i])))
-        print(arr)
         ans=[]
         c=0
         for j in range(len(arr)):
             for i in range(len(matrix)):
-                print(matrix[i][arr[j]])
                 if matrix[j][arr[j]]>matrix[i][arr[j]]:
                     c+=1
             if c==len(arr)-1:
                 return [matrix[j][arr[j]]]
             c=0
-
         return []
