21ae9de2570937615caaf2e52b70e2feebc876af
Maulik Vaghela <maulik.vagh@gmail.com>
2024-07-23 21:01:30 -0400

    Time: 1084 ms (58.15%), Space: 29.2 MB (5.98%) - LeetHub

--- 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.py
+++ 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.py
@@ -4,23 +4,47 @@ class Solution:
         for i in range(len(nums)):
             st.append(str(nums[i]))
 
-        # arr=[]
         d=defaultdict()
 
         for s in range(len(st)):
-            # n=str(mapping[int(st[s][0])])
             p=mapping[int(st[s][0])]
             for c in range(1,len(st[s])):
-                # print(s,c, int(st[s][c]))
                 p=p*(10)+mapping[int(st[s][c])]
-                # n+=str(mapping[int(st[s][c])])
-            # arr.append(p)
             d[int(st[s])]=p
-            # print(arr)
-            # print(p)
-        # for i in range(len(nums)):
-            # d[nums[i]]=arr[i]
+
         if max(d.values())==min(d.values()):
             return nums
+
         nums.sort(key=lambda x: (d[x], -x))
         return nums
+
+
+
+
+
+
+
+        # st=[]
+        # for i in range(len(nums)):
+        #     st.append(str(nums[i]))
+
+        # # arr=[]
+        # d=defaultdict()
+
+        # for s in range(len(st)):
+        #     # n=str(mapping[int(st[s][0])])
+        #     p=mapping[int(st[s][0])]
+        #     for c in range(1,len(st[s])):
+        #         # print(s,c, int(st[s][c]))
+        #         p=p*(10)+mapping[int(st[s][c])]
+        #         # n+=str(mapping[int(st[s][c])])
+        #     # arr.append(p)
+        #     d[int(st[s])]=p
+        #     # print(arr)
+        #     # print(p)
+        # # for i in range(len(nums)):
+        #     # d[nums[i]]=arr[i]
+        # if max(d.values())==min(d.values()):
+        #     return nums
+        # nums.sort(key=lambda x: (d[x], -x))
+        # return nums
