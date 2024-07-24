627f0ce5904f273e19d65daeda62ad07bbe1e275
Maulik Vaghela <maulik.vagh@gmail.com>
2024-07-23 20:43:01 -0400

    Time: 1462 ms (10.32%), Space: 28.3 MB (6.52%) - LeetHub

--- 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.py
+++ 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.py
@@ -1,13 +1,9 @@
 class Solution:
     def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
         st=[]
-        if nums==[9,99,999,9999,999999999]:
-            return [9,99,999,9999,999999999]
         for i in range(len(nums)):
             st.append(str(nums[i]))
-        print(st)
         arr=[]
-        arr1=[]
         for s in range(len(st)):
             n=''
             n1=''
@@ -16,13 +12,9 @@ class Solution:
                 n+=str(mapping[int(st[s][c])])
                 n1+=str(c)
             arr.append(int(n))
-            arr1.append(int(n1))
-        print(arr)
-        print(arr1)
         d=defaultdict(list)
         for i in range(len(nums)):
             d[nums[i]]=arr[i]
-        ans=[]
         if max(arr)==min(arr):
             return nums
         nums.sort(key=lambda x: (d[x], -x))
