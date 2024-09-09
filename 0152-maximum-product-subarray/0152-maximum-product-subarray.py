a8160369c418add71f680a7cedf7fc6611d690c6
Maulik Vaghela <maulik.vagh@gmail.com>
2024-09-09 11:51:18 -0400

    Time: 66 ms (85.33%), Space: 17.1 MB (30.45%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,15 +1,33 @@
 import math
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        ans=float('-inf')
-        ans=max(ans,math.prod(nums))
-        if ans>0:
-            return ans
-        ans=max(ans,max(nums))
+        #This is an Example of Dynamic Programming
+        #start with first element of the array
+        curr_min=nums[0]
+        curr_max=nums[0]
         
-        for i in range(len(nums)-1):
-            for j in range(i+1, len(nums)):
-                # print(nums[i:j+1])
-                ans=max(ans,math.prod(nums[i:j+1]))
+        ans=curr_max
         
+        #start comparing with next element
+        for num in nums[1:]:
+            temp_min=min(num,num*curr_min,num*curr_max)
+            curr_max=max(num,num*curr_min,num*curr_max)
+            curr_min=temp_min
+            
+            ans=max(ans,curr_max)
         return ans
+    
+#         Using Brute Force
+#         # Runtime: 260 ms
+#         ans=float('-inf')
+#         ans=max(ans,math.prod(nums))
+#         if ans>0:
+#             return ans
+#         ans=max(ans,max(nums))
+        
+#         for i in range(len(nums)-1):
+#             for j in range(i+1, len(nums)):
+#                 # print(nums[i:j+1])
+#                 ans=max(ans,math.prod(nums[i:j+1]))
+        
+#         return ans
