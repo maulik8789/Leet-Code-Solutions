89c4948b65379324d689fed5691b4b4036ba0e48
Maulik Vaghela <maulik.vagh@gmail.com>
2024-09-10 23:53:25 -0400

    Time: 37 ms (38.86%), Space: 16.6 MB (32.76%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,20 +1,33 @@
 class Solution:
     def decodeString(self, s: str) -> str:     
+        """
+        #Runtime: 23 ms, faster than 98.36% of Python3
+        # This is an example of Stack
+        """
         dig=0
         stck=[['',1]]
         
         for c in s:
+            # if character is digit, keep adding numbers
             if c.isdigit():
                 dig = dig*10 + int(c)
             
+            # if character is an alphabet keep adding in string
             elif c.isalpha():
                 stck[-1][0]+=c
                 
+            # when bracket starts add an empty string and 
+            # the dig gathered to stack    
             elif c == '[':
                 stck.append(['',dig])
+                #empty the digit after it is stored
                 dig=0
                 # print(stck)
                 
+            # when the bracket is closed, get last element of Stack which is the string and digit.
+            # remove(pop) them from the stack and add the multiplication of that string with digit to 
+            # 'new' last string of the Stack
+
             elif c==']' :
                 # print(stck)
                 prev_str,prev_dig=stck.pop()
@@ -23,4 +36,5 @@ class Solution:
                 stck[-1][0]+=(prev_str * prev_dig)
                 # print(stck)
 
+        # return the string part from the Stack      
         return stck[0][0]
