f471d86671f1df1ed8a72cd824baa5f429994e7c
Maulik Vaghela <maulik.vagh@gmail.com>
2024-09-18 12:21:15 -0400

    Time: 23 ms (98.44%), Space: 16.6 MB (33.66%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.py
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.py
@@ -1,13 +1,13 @@
 class Solution:
     def checkRecord(self, s: str) -> bool:
-        d=defaultdict()
-        d['A']=0
-        for c in s:
-            if c=='A':
-                d['A']+=1
-        
+        # d=defaultdict()
+        # d['A']=0
+        # for c in s:
+        #     if c=='A':
+        #         d['A']+=1
+        # print()
         if len(s)<2:
             return True
-        if d['A']>=2 or 'LLL' in s or ('P' not in s and 'L' not in s):
+        if Counter(s)['A']>=2 or 'LLL' in s or ('P' not in s and 'L' not in s):
             return False
         return True
