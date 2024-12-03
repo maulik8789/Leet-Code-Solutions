79df207d3e93956540c408b2eb8d47e8d321e5af
Maulik Vaghela <maulik.vagh@gmail.com>
2024-12-02 22:14:10 -0500

    Time: 41 ms (18.33%), Space: 18.6 MB (19.40%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        arr = [0] * (len(nums))
+        print(arr)
+        pointer = defaultdict(int)
+        for i in range(len(nums)):
+            pointer[nums[i]] += 1 
+            arr[nums[i]-1] = nums[i]
+        # print(arr)
+        ans1 = 0
+        for key, val in pointer.items():
+            if val == 2:
+                ans1 = key 
+        return [ans1, arr.index(0)+1]
