a4356e9dfecb8828d9ce06f892cd470b157e432d
Maulik Vaghela <maulik.vagh@gmail.com>
2024-12-06 08:22:21 -0500

    Time: 0 ms (100.00%), Space: 25.2 MB (25.36%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,6 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(k):
-            nums.insert(0, nums.pop(-1))
+        # for i in range(k):
+        #     nums.insert(0, nums.pop(-1))
+        # print(k%len(nums))
+        n = len(nums)
+        k = k % n
+        nums.reverse()
+        nums[:k]=nums[:k][::-1]        
+        # print(nums)
+        nums[k-n:]=nums[k-n:][::-1]
+
         
