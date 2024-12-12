b9f4a7eabab52988ee31f62ccf02ff0e7757f968
Maulik Vaghela <maulik.vagh@gmail.com>
2024-12-12 09:23:55 -0500

    Time: 23 ms (37.01%), Space: 17.4 MB (20.60%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
@@ -1,15 +1,11 @@
 import math
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
-        tot = 0
-        d = defaultdict(int)
-        
         while k > 0:
             k-=1
             gifts.sort(reverse = True)
-            # gifts = gifts[::-1]
             sqRoot = math.floor(math.sqrt(gifts[0]))
-            gft = sqRoot
-            gifts[0] = gft
+            # gft = sqRoot
+            gifts[0] = sqRoot
 
         return sum(gifts)
