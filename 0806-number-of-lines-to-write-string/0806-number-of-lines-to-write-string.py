a77b38ac68fcdf5c3d2bd122c6989c1a2a2bbb09
Maulik Vaghela <maulik.vagh@gmail.com>
2024-12-20 13:13:25 -0500

    Time: 38 ms (46.34%), Space: 18 MB (7.18%) - LeetHub

--- 0806-number-of-lines-to-write-string/0806-number-of-lines-to-write-string.py
+++ 0806-number-of-lines-to-write-string/0806-number-of-lines-to-write-string.py
@@ -2,12 +2,10 @@ import string
 
 class Solution:
     def numberOfLines(self, widths: List[int], s: str) -> List[int]:
-        # alph = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
         alph = 'abcdefghijklmnopqrstuvwxyz'
         lines = 0
         tot = 0
         for l in range(len(s)):
-            
             ind = alph.index(s[l])
             
             if tot + widths[ind] > 100:
