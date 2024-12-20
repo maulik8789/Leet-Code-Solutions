d8a517d0d5de26cf91a633199fdcb841b5187101
Maulik Vaghela <maulik.vagh@gmail.com>
2024-12-20 13:11:46 -0500

    Time: 47 ms (6.62%), Space: 17.9 MB (7.18%) - LeetHub

--- 0806-number-of-lines-to-write-string/0806-number-of-lines-to-write-string.py
+++ 0806-number-of-lines-to-write-string/0806-number-of-lines-to-write-string.py
@@ -1,11 +1,14 @@
+import string
+
 class Solution:
     def numberOfLines(self, widths: List[int], s: str) -> List[int]:
-        alph = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+        
         lines = 0
         tot = 0
+        
         for l in range(len(s)):
             
-            ind = alph.index(s[l])
+            ind = string.ascii_lowercase.index(s[l])
             
             if tot + widths[ind] > 100:
                 tot = 0
@@ -13,5 +16,19 @@ class Solution:
                 tot += widths[ind]
             else:
                 tot += widths[ind]
-            # print(tot, ind,alph[ind], lines, widths[ind])
         return [lines + 1, tot]
+        # Runtime: 36 ms, faster than 57.89% of Python3 online submissions
+#         alph = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+#         lines = 0
+#         tot = 0
+#         for l in range(len(s)):
+            
+#             ind = alph.index(s[l])
+            
+#             if tot + widths[ind] > 100:
+#                 tot = 0
+#                 lines += 1
+#                 tot += widths[ind]
+#             else:
+#                 tot += widths[ind]
+#         return [lines + 1, tot]
